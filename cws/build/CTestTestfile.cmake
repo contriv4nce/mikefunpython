762a6b0db070f7c6cf0bda65660d26a34d440612
mikewiltero <mike@kr-tech.net>
2016-04-15 16:13:36 -0400

    added catkin workspace "cws"

---
+++ cws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mike/code/mikefunpython/cws/src
+# Build directory: /home/mike/code/mikefunpython/cws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ cws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mike/code/mikefunpython/cws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/mike/code/mikefunpython/cws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/mike/code/mikefunpython/cws/devel/lib:/home/mike/code/mikefunpython/cws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/mike/code/mikefunpython/cws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/mike/code/mikefunpython/cws/devel/lib/pkgconfig:/home/mike/code/mikefunpython/cws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/mike/code/mikefunpython/cws/build"
+export PYTHONPATH="/home/mike/code/mikefunpython/cws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mike/code/mikefunpython/cws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/mike/code/mikefunpython/cws/src:$ROS_PACKAGE_PATH"
